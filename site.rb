91dd51e47f4bf776c8a9cb9ba621e04a27f5bcad
Ismael Celis <ismaelct@gmail.com>
2010-11-19 02:14:39 -0300

    Ajax messages to app

--- site.rb
+++ site.rb
@@ -3,7 +3,7 @@ require "bundler/setup"
 
 require 'sinatra/base'
 require 'sinatra/content_for'
-
+require 'pusher'
 require 'config'
 
 class Site < Sinatra::Base
@@ -12,10 +12,21 @@ class Site < Sinatra::Base
   set     :root, File.dirname(__FILE__)
   set     :public, Proc.new { File.join(root, "public") }
   
+  configure do
+    # configure with your own app http://pusherapp.com
+    Pusher.app_id = '2860'
+    Pusher.key = 'cd9dd4f9a0f97e3a5bf1'
+    Pusher.secret = '7a25be1f704e5be98ac1'
+  end
+  
   helpers Sinatra::ContentFor
   
   get '/?' do
     erb :pusher_chat
+  end
+  
+  post '/messages' do
+    Pusher['chat'].trigger('message', params[:message])
   end
 
 end
