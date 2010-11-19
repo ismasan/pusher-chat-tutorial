3480a0372c1e53fbd525cfdb11a1d8ba4adefa7f
Ismael Celis <ismaelct@gmail.com>
2010-11-19 03:21:50 -0300

    Presence chat, with gravatars!

--- site.rb
+++ site.rb
@@ -5,8 +5,15 @@ require 'sinatra/base'
 require 'sinatra/content_for'
 require 'pusher'
 require 'config'
+require 'digest/md5'
 
-class User < Struct.new(:user_name, :email);end
+class User < Struct.new(:user_name, :email)
+  
+  def gravatar_url(size=40)
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}.png?s=#{size}"
+  end
+  
+end
 
 class Site < Sinatra::Base
   
@@ -53,13 +60,19 @@ class Site < Sinatra::Base
   post '/messages' do
     message = params[:message]
     message[:user] = current_user.user_name
-    Pusher['private-chat'].trigger('message', message)
+    Pusher['presence-chat'].trigger('message', message)
   end
   
   post '/pusher/auth' do
     if current_user
       auth = Pusher[params[:channel_name]].authenticate(params[:socket_id],
-        :user_id => current_user.email
+        # mandatory
+        :user_id => current_user.email,
+        # optional
+        :user_info => {
+          :user_name   => current_user.user_name, 
+          :gravatar_url => current_user.gravatar_url
+        }
       )
       content_type 'application/json'
       Pusher::JSON.generate auth
